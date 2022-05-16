38c611660a4198923347320f9475dfdb5436e1fa
kellyzhangg <kellyzhangg12@gmail.com>
2022-05-16 18:43:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React from 'react';
 import './App.css';
+import Weather from './components/Header';
+import Location from './components/Location';
 
-function App() {
+
+const api = {
+  key: 'a1ff59fe7bfbac4b4a1bbc981c33e490',
+  base: 'http://api.openweathermap.org/geo/1.0/direct?q='
+}
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <div className = "Title">
+        <Weather/>
+      </div>
+      <div className = "Input">
+        <Location/>
+      </div>
     </div>
   );
 }
+
 
 export default App;
